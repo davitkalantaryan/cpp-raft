814695c97c66d76b46dc68b8ce21120f58077596
davitkalantaryan <davit.kalantaryandesy.de>
2018-09-01 09:09:12 +0200

    added args to client main

--- src/test/main_tcp_client_test.cpp
+++ src/test/main_tcp_client_test.cpp
@@ -6,15 +6,17 @@
 #define CLIENT_VERSION  "test client version 1.0.0"
 
 
-int main()
+int main( int argc, char* argv[])
 {
     int nReturn;
     ::common::SocketTCP aSocket;
 
+    if(argc<2){return 1;}
+
     ::common::socketN::Initialize();
     ::std::cout<< CLIENT_VERSION << ::std::endl;
 
-    nReturn = aSocket.connectC("kalantar.pagekite.me",9030,2000);
+    nReturn = aSocket.connectC(argv[1],9030,2000);
     if(nReturn){goto returnPoint; }
 
     aSocket.writeC(CLIENT_VERSION,strlen(CLIENT_VERSION));
